4d4e3039437960704eb6c32e410e97d13b2f722e
Code Ass <aycabta@gmail.com>
2014-05-04 19:32:36 +0900

    move crawl feature from crawl.rb to model.rb

--- model.rb
+++ model.rb
@@ -1,6 +1,10 @@
 require 'bundler'
 require 'dm-core'
 require 'dm-migrations'
+require 'feedjira'
+require 'digest/sha1'
+require 'net/http'
+require 'uri'
 
 class Room
   include DataMapper::Resource
@@ -20,6 +24,24 @@ class Feed
   has n, :connections
   has n, :rooms, :through => :connections
   has n, :entries
+
+  def self.crawl
+    all.each do |feed|
+      feedjira = Feedjira::Feed.fetch_and_parse feed.url
+      title = feedjira.entries.first.title
+      url = feedjira.entries.first.url
+      published = feedjira.entries.first.published
+      if Entry.first({:url => url, :published => published}).nil?
+        entry = Entry.create(:title => title, :url => url, :published => published, :feed => feed)
+        feed.connections.each do |connection, room = connection.room|
+        text = "#{title}\n#{url}"
+        request_url = "http://lingr.com/api/room/say?room=#{URI.encode(room.room_id)}&bot=#{URI.encode(ENV["BOT_ID"])}&text=#{URI.encode(text)}&bot_verifier=#{URI.encode(Digest::SHA1.hexdigest(ENV["BOT_ID"] + ENV["BOT_SECRET"]))}"
+        uri = URI.parse(request_url)
+        response = Net::HTTP.get_response(uri)
+        end
+      end
+    end
+  end
 end
 
 class Connection
