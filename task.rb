0149bd161a3b9334a91211c310ce43d10ec3a4ae
Code Ass <aycabta@gmail.com>
2016-01-09 02:57:09 +0900

    Remove Sinatra initialization code from task.rb

--- task.rb
+++ task.rb
@@ -1,15 +1,8 @@
 require 'bundler'
 require './model'
 
-configure :production do
-  DataMapper.setup(:default, ENV["DATABASE_URL"])
-  database_upgrade!
-end
-
-configure :test, :development do
-  DataMapper.setup(:default, "yaml:///tmp/feed2lingr")
-  database_upgrade!
-end
+DataMapper.setup(:default, ENV["DATABASE_URL"])
+database_upgrade!
 
 Feed.crawl
 
